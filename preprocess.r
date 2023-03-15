5c63bf7d748612f2d0ce40af7279f51893c670ba
Alparslan Özen <ozen_alparslan@outlook.com>
2023-03-16 00:11:51 +0300

    Update preprocess.r
    - Delete empty rows
    - Calculate ride_length and day_of_week

--- preprocess.r
+++ preprocess.r
@@ -13,11 +13,16 @@ empty_cells <- colSums(is.na(data) | data == "" | is.null(data))
 percent_empty <- round(empty_cells / nrow(data) * 100, 2)
 print(percent_empty)
 
-data_new <- subset(data, select = -c(start_station_name,
-    start_station_id, end_station_name, end_station_id))
-
-
 # Remove rows where no info exists
-data_complete <- data_new[rowSums(is.na(data_new) | data_new == "") == 0, ]
+data <- data[rowSums(is.na(data) | data == "") == 0, ]
 
-write.csv(data_complete, "cleaned_data.csv", row.names = FALSE)
+# Convert "started_at" and "ended_at" columns to datetime since,
+# we can not operate on chr
+data$started_at <- as.POSIXct(data$started_at, format = "%Y-%m-%d %H:%M:%S")
+data$ended_at <- as.POSIXct(data$ended_at, format = "%Y-%m-%d %H:%M:%S")
+# Create a new column for the ride length
+data$ride_length <- difftime(data$ended_at, data$started_at, units = "mins")
+# Create a new column for the day of the week
+data$day_of_week <- as.numeric(format(data$started_at, "%u"))
+
+write.csv(data, "processed_data.csv", row.names = FALSE)
