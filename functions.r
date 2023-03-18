4459f385ed3910150f813dd4caf2f74f1c5b00c9
Alparslan Özen <ozen_alparslan@outlook.com>
2023-03-19 00:27:59 +0300

    Create functions.r
    Created a separate functions script to store and call functions from

---
+++ functions.r
@@ -0,0 +1,19 @@
+check_consistency <- function(df_list) {
+  # Get the column names and types of the first data frame
+  col_names <- names(df_list[[1]])
+  col_types <- sapply(df_list[[1]], class)
+  # Check if the column names and types are consistent in all data frames
+  for (i in 2:length(df_list)) {
+    if (!identical(col_names, names(df_list[[i]])) ||
+        !identical(col_types, sapply(df_list[[i]], class))) {
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
+
+
+# Check the structure of data
+get_data_str <- function(dataframe) {
+  capture.output(str(dataframe, max.level =1))
+}
